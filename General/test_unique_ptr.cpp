06fdb4df09e37811f98ad7974da9813c3d0eccde
claudehang <2392292107@qq.com>
2018-07-08 23:13:17 +0800

    Update test_unique_ptr.cpp

--- General/test_unique_ptr.cpp
+++ General/test_unique_ptr.cpp
@@ -3,13 +3,18 @@
 #include <cassert>
 
 int main() {
+	// initialize an array that contains 4 unique_ptr objects
+	// which are all set to nullptr
 	std::unique_ptr<int> uptr[4];
 	for (int i = 0; i < 4; i++) {
 		assert(uptr[i] == nullptr);
+		// add something into each unique_ptr object
 		uptr[i] = std::unique_ptr<int>(new int(i));
 	}
+	// print out if we have made the changes as expected
 	for (int j = 0; j < 4; j++) {
 		std::cout << *uptr[j].get() << std::endl;
 	}
+	// freeze and wait for any keyborad input with ENTER to shut down
 	std::cin.get();
 }
